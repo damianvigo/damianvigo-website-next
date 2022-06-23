d6ebd6461cfda28693fc3da81d919bae091db281
Damian Vigo <damiandanielvigo@gmail.com>
2022-06-23 10:38:02 -0300

    fix-btn-scroll

--- hooks/useScrollTop.js
+++ hooks/useScrollTop.js
@@ -1,20 +1,28 @@
 import { useEffect, useState } from 'react';
 
 const useScrollTop = () => {
-  const [scrollTop, setScrollTop] = useState('');
+  const [scrollTop, setScrollTop] = useState('hidden');
 
   useEffect(() => {
     const d = document,
       w = window;
-    w.addEventListener('scroll', (e) => {
-      let scrollTop = w.pageYOffset || d.documentElementScrollTop;
 
-      if (scrollTop > 300) {
-        setScrollTop(null);
-      } else {
-        setScrollTop('hidden');
-      }
-    });
+    const top = () => {
+      w.addEventListener('scroll', (e) => {
+        let scrollTop = w.pageYOffset || d.documentElementScrollTop;
+        // console.log(scrollTop);
+        if (scrollTop > 300) {
+          setScrollTop(null);
+        } else {
+          setScrollTop('hidden');
+        }
+      });
+    };
+
+    top();
+    return () => {
+      w.removeEventListener('scroll', top);
+    };
   }, [scrollTop, setScrollTop]);
 
   const toTop = (e) => {
