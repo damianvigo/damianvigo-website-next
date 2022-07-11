ee614150bc54e364181f17042ff593c1d48bfd30
Damian Vigo <damiandanielvigo@gmail.com>
2022-07-11 19:08:34 -0300

    mod-ga

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@ import '../styles/globals.css';
 import 'animate.css/animate.min.css';
 import 'hamburgers/dist/hamburgers.min.css';
 import { useRouter } from 'next/router';
-import { GoogleAnalytics, usePageViews } from 'nextjs-google-analytics';
 import { ThemeProvider } from '../context/ThemeContext';
+import Script from 'next/script';
 import NextNProgress from 'nextjs-progressbar';
 import BtnMusic from '../utils/BtnMusic';
 
@@ -15,7 +15,23 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <ThemeProvider>
-        <GoogleAnalytics />
+        <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=UA-145056950-1"
+        />
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145056950-1"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'UA-145056950-1');
+          </script>
+          
+          `}
+        </Script>
         {pathname !== '/404' && <BtnMusic />}
         <NextNProgress color="#f72585" />
         <Component {...pageProps} />
