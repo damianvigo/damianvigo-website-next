e18a1bd4fd45d69230f689222278f6015dc318b0
Damian Vigo <damiandanielvigo@gmail.com>
2022-07-11 18:51:26 -0300

    fix-ga

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,20 @@ import '../styles/globals.css';
 import 'animate.css/animate.min.css';
 import 'hamburgers/dist/hamburgers.min.css';
 import { useRouter } from 'next/router';
-import { GoogleAnalytics } from 'nextjs-google-analytics';
+import { GoogleAnalytics, usePageViews } from 'nextjs-google-analytics';
 import { ThemeProvider } from '../context/ThemeContext';
 import NextNProgress from 'nextjs-progressbar';
 import BtnMusic from '../utils/BtnMusic';
 
 function MyApp({ Component, pageProps }) {
+  usePageViews();
   const router = useRouter();
   const { pathname } = router;
-
-  const id = process.env.GOOGLE_ANALYTICS;
 
   return (
     <>
       <ThemeProvider>
-        <GoogleAnalytics gaMeasurementId="UA-145056950-1" />
+        <GoogleAnalytics />
         {pathname !== '/404' && <BtnMusic />}
         <NextNProgress color="#f72585" />
         <Component {...pageProps} />
