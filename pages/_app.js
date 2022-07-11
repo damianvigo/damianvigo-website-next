c2371fbc7a4d278ddb6814f493cc53f1ff125c52
Damian Vigo <damiandanielvigo@gmail.com>
2022-07-11 15:41:39 -0300

    add-gtm

--- pages/_app.js
+++ pages/_app.js
@@ -12,20 +12,6 @@ function MyApp({ Component, pageProps }) {
   const { pathname } = router;
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
-      />
-
-      <Script id="google-analytics" strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', ${process.env.GOOGLE_ANALYTICS});
-          `}
-      </Script>
       <ThemeProvider>
         {pathname !== '/404' && <BtnMusic />}
         <NextNProgress color="#f72585" />
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   const path = process.env.DOMINIO;
@@ -62,10 +63,44 @@ export default function Document() {
           name="twitter:url"
           content="https://damianvigo.com"
         />
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+        />
+
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.GOOGLE_ANALYTICS});
+          `}
+        </Script>
+        <Script id="google-tag-manager-head">
+          {`
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TD8JFJT');</script>
+        <!-- End Google Tag Manager -->
+        
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
         <NextScript />
+        <Script id="google-tag-manager-body">
+          {`
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TD8JFJT"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        `}
+        </Script>
       </body>
     </Html>
   );
