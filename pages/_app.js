c8a793ab86bb7e7f014123005c5dd796cf1fcf68
Damian Vigo <damiandanielvigo@gmail.com>
2022-07-11 16:15:57 -0300

    move-ga-gtm

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,32 @@ function MyApp({ Component, pageProps }) {
   const { pathname } = router;
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.GOOGLE_ANALYTICS});
+          `}
+      </Script>
+      <Script id="google-tag-manager-head">
+        {`
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TD8JFJT');</script>
+        <!-- End Google Tag Manager -->
+        
+        `}
+      </Script>
       <ThemeProvider>
         {pathname !== '/404' && <BtnMusic />}
         <NextNProgress color="#f72585" />
--- pages/_document.js
+++ pages/_document.js
@@ -63,32 +63,6 @@ export default function Document() {
           name="twitter:url"
           content="https://damianvigo.com"
         />
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
-        />
-
-        <Script id="google-analytics" strategy="lazyOnload">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', ${process.env.GOOGLE_ANALYTICS});
-          `}
-        </Script>
-        <Script id="google-tag-manager-head">
-          {`
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TD8JFJT');</script>
-        <!-- End Google Tag Manager -->
-        
-        `}
-        </Script>
       </Head>
       <body>
         <Main />
